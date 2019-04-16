7f8f5014e7d9ab6c96901d08a2e6f3069545e3d6
Alexander Mancevice <amancevice@cargometrics.com>
2019-04-16 16:51:12 -0400

    Update requirements and setup

--- setup.py
+++ setup.py
@@ -1,22 +1,31 @@
 from setuptools import setup
+from setuptools import find_packages
 
-setup(name='redpanda',
-      use_scm_version=True,
-      author='amancevice',
-      author_email='smallweirdnum@gmail.com',
-      packages=['redpanda'],
-      url='http://www.smallweirdnumber.com',
-      description='Pandas-ORM Integration.',
-      long_description='See GitHub_ for documentation.'
-                       '.. _GitHub: https://github.com/amancevice/redpanda',
-      classifiers=['Intended Audience :: Developers',
-                   'License :: OSI Approved :: MIT License',
-                   'Operating System :: OS Independent',
-                   'Programming Language :: Python :: 2',
-                   'Programming Language :: Python :: 2.7',
-                   'Programming Language :: Python :: 3',
-                   'Programming Language :: Python :: 3.6',
-                   'Programming Language :: Python',
-                   'Topic :: Utilities'],
-      setup_requires=['setuptools_scm'],
-      install_requires=['pandas >= 0.16.0', 'sqlalchemy >= 1.1.10'])
+
+def requirements(path):
+    with open(path) as req:
+        return [x.strip() for x in req.readlines() if not x.startswith('-')]
+
+
+setup(
+    author='amancevice',
+    author_email='smallweirdnum@gmail.com',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python',
+        'Topic :: Utilities',
+    ],
+    description='Pandas-ORM Integration.',
+    install_requires=requirements('requirements.txt'),
+    name='redpanda',
+    packages=find_packages(exclude='tests'),
+    setup_requires=['setuptools_scm'],
+    url='https://github.com/amancevice/redpanda',
+    use_scm_version=True,
+)
