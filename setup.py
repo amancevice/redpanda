afeaae0eb63bd23dcddb5efaf00717c89e999638
Alexander Mancevice <amancevice@cargometrics.com>
2019-04-20 09:24:25 -0400

    Relax setup requirements

--- setup.py
+++ setup.py
@@ -1,17 +1,14 @@
 from setuptools import setup
 from setuptools import find_packages
 
-
-def requirements(path):
-    with open(path) as req:
-        return [x.strip() for x in req.readlines() if not x.startswith('-')]
-
-
 setup(
     author='amancevice',
     author_email='smallweirdnum@gmail.com',
     description='Pandas-ORM Integration.',
-    install_requires=requirements('requirements.txt'),
+    install_requires=[
+        'pandas >= 0.16',
+        'sqlalchemy >= 1.3',
+    ],
     name='redpanda',
     packages=find_packages(exclude=['tests']),
     setup_requires=['setuptools_scm'],
