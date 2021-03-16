cbb704a507f475c6b2c20c9fefc4a7a92a9c1c59
Alexander Mancevice <alexander.mancevice@hey.com>
2021-03-16 19:23:41 -0400

    setup.py fix

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
-from setuptools import find_packages
-from setuptools import setup
+from setuptools import (find_packages, setup)
 
 with open('README.md', 'r') as readme:
     long_description = readme.read()
