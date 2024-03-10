68729aeeebd37b095d8227910fd6ea383040969c
Alexander Mancevice <alexander.mancevice@hey.com>
2024-03-10 11:32:33 -0400

    ~> v0.6

--- redpanda/__init__.py
+++ redpanda/__init__.py
@@ -5,4 +5,4 @@ Pandas-ORM Integration.
 from sqlalchemy import create_engine  # noqa: F401
 from redpanda.orm import Query, Session, sessionmaker  # noqa: F401
 
-__version__ = "0.5.1"
+__version__ = "0.6.0"
